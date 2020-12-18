8ed7bfac40b3497e45329b222cfb6ef776966d3a
xieTY <spandelaki@unikadelasalle.ac.id>
2020-12-19 01:27:19 +0800

    init

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -21,7 +21,7 @@
             <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
               <div class="pr-12">
                 <h1 class="text-white font-semibold text-4xl">
-                  PT. Blesindo Palma Cemerlang
+                  PT. Blesindo Palma Cleon
                 </h1>
                 <p class="mt-4 text-lg text-gray-300">
                   Your Security is Our Priority
